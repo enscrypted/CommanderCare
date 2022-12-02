c3cfd913c7c7355e1531c778ef376079302f34f7
enscrypted <112054797+enscrypted@users.noreply.github.com>
2022-12-01 22:55:08 -0500

    Fixing http server

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// port setup
+app.set('port', process.env.PORT || 3000);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
