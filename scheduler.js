bcedbd32858afe057d9908ead8bf904cc6960f8a
enscrypted <112054797+enscrypted@users.noreply.github.com>
2024-05-15 21:30:22 -0400

    test 2

--- scheduler.js
+++ scheduler.js
@@ -15,7 +15,7 @@ var transporter = nodemailer.createTransport(smtpTransport({
 }));
 
 // auto clocking out employees that forgot 12AM EST every day
-cron.schedule('30 1 * * *', async () => {
+cron.schedule('31 1 * * *', async () => {
   await autoClockOut();
 });
 
